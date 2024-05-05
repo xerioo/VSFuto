b487e1cc26b62c3d7c549e13ff81008ee29ea321
xeri <xerioo@gmail.com>
2024-05-05 08:19:18 +0200

    az előbb nem ment fel semmi

--- src/main/java/GDE/futo/service/RunnerService.java
+++ src/main/java/GDE/futo/service/RunnerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import GDE.futo.DatabaseConnection;
+import GDE.futo.FutoApplication;
 import GDE.futo.Runner;
 
 import java.util.HashMap;
@@ -14,11 +15,12 @@ import java.util.Map;
 public class RunnerService {
 
     @Autowired
-    public void getRunnersByCompetition(int id) {
+    public List<Runner> getRunnersByCompetition(int id) {
             String sql = "SELECT * FROM RUNNERDB INNER JOIN RESULTDB on RUNNERDB.ID=RESULTDB.RUNNER " + 
                                 "where RESULTDB.COMPETITION=" + id +
-                                " ORDER BY RESULTDB.TIME";          
+                                " ORDER BY RESULTDB.TIME";       
             DatabaseConnection.getQuery(sql);
-
+            List<Runner> runners = FutoApplication.runners;
+        return runners;
     }
 }
